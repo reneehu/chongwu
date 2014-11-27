98d4e2cfaa1f03b6a82237e2ef9ef35b60e5b3be
dongyang zhao <michaelzhao007@hotmail.com>
2014-11-27 18:38:36 -0500

    six

--- web.js
+++ web.js
@@ -1,10 +1,9 @@
 /**
  * Created by michaelzhao007 on 11/27/2014.
  */
-var gzippo = require('gzippo');
+
 var express = require('express');
 var app = express();
 
 app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
